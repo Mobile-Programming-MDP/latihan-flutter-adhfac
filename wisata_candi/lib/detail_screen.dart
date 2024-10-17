150773a6f385c2e9d78f6a5e923bb1c92e89720f
adhfac <mdhafaadjiesaputra_2327250081@mhs.mdp.ac.id>
2024-10-17 15:10:57 +0700

    add detail header

---
+++ wisata_candi/lib/detail_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/models/candi.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Candi candi;
+  const DetailScreen({super.key, required this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Padding(padding: const EdgeInsets.symmetric(
+                horizontal: 8.0
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(candi.imageAsset,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+                ),
+              ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 32
+              ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle
+                  ),
+              ),
+              )
+            ],
+          ),
+          Text(candi.name),
+          Text(candi.description)
+        ],
+      ),
+    );
+  }
+}
