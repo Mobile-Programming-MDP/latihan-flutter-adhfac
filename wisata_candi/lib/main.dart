d16d7e7a4ece968b047897fda91f949546add8b8
adhfac <mdhafaadjiesaputra_2327250081@mhs.mdp.ac.id>
2024-11-09 12:51:45 +0700

    wiscandi-241109

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/detail_screen.dart';
 import 'package:wisata_candi/profilescreen.dart';
+import 'package:wisata_candi/search_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -16,7 +17,7 @@ class MainApp extends StatelessWidget {
       // home: DetailScreen(
       //   candi: candiList[0],
       // ),
-      home: Profilescreen(),
+      home: SearchScreen(),
     );
   }
 }
